0041a075c75c60fc30a8f9521147500309801617
xianning liu <xianlinbox@gmail.com>
2024-03-03 09:02:42 -0600

    update metadata of embeddings

--- app/chat/create_embeddings.py
+++ app/chat/create_embeddings.py
@@ -8,4 +8,10 @@ def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     loader = PyPDFLoader(file_path=pdf_path)
     texts = loader.load_and_split(text_splitter=text_splitter)
 
+    for text in texts:
+        text.metadata = {
+            "pdf-id": pdf_id,
+            "page": text.metadata["page"],
+            "text": text.page_content,
+        }
     vector_store.add_documents(texts)
