0994e65ff6c8bf5f2b7b47f36af323ea49852543
xianning liu <xianlinbox@gmail.com>
2024-03-03 08:46:20 -0600

    fix the import issue

--- app/chat/vector_store/pinecone.py
+++ app/chat/vector_store/pinecone.py
@@ -1,7 +1,7 @@
 import os
 import pinecone
 from langchain.vectorstores.pinecone import Pinecone
-from app.chat.embeddings import embeddings
+from app.chat.embeddings.openai import embeddings
 
 pinecone.init(
     api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENV_NAME")
