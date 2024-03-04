1c1619ee864e575049c5d4c87163641b46c77c46
xianning liu <xianlinbox@gmail.com>
2024-03-04 09:16:37 -0600

    add sql message history

---
+++ app/chat/memories/sql_message_memory.py
@@ -0,0 +1,22 @@
+from langchain.schema import BaseChatMessageHistory
+from langchain_core.messages import BaseMessage
+
+from app.web.api import get_messages_by_conversation_id, add_message_to_conversation
+
+
+class SqlMessageHistory(BaseChatMessageHistory):
+    conversation_id: str
+
+    @property
+    def message(self):
+        return get_messages_by_conversation_id(self.conversation_id)
+
+    def add_message(self, message: BaseMessage) -> None:
+        return add_message_to_conversation(
+            conversation_id=self.conversation_id,
+            role=message.type,
+            content=message.content,
+        )
+
+    def clear(self) -> None:
+        return super().clear()
