f1b1921dadbcc3289218213ad56ffbf41bd4e9b1
xianning liu <xianlinbox@gmail.com>
2024-03-04 09:21:54 -0600

    add build_memory

--- app/chat/memories/sql_message_memory.py
+++ app/chat/memories/sql_message_memory.py
@@ -1,11 +1,18 @@
 from langchain.schema import BaseChatMessageHistory
 from langchain_core.messages import BaseMessage
+from langchain.memory import ConversationBufferMemory
+from app.chat.models import ChatArgs
 
 from app.web.api import get_messages_by_conversation_id, add_message_to_conversation
 
 
 class SqlMessageHistory(BaseChatMessageHistory):
+
     conversation_id: str
+
+    def __init__(self, conversation_id) -> None:
+        self.conversation_id = conversation_id
+        super().__init__()
 
     @property
     def message(self):
@@ -20,3 +27,12 @@ class SqlMessageHistory(BaseChatMessageHistory):
 
     def clear(self) -> None:
         return super().clear()
+
+
+def build_memory(chat_args: ChatArgs):
+    return ConversationBufferMemory(
+        chat_memory=SqlMessageHistory(conversation_id=chat_args.conversation_id),
+        return_messages=True,
+        input_key="chat_history",
+        output_key="answer",
+    )
