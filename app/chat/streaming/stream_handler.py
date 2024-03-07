1c4c2495c6fe2b80cdbde61dc0e3b5e27f4cce53
xianning liu <xianlinbox@gmail.com>
2024-03-06 19:00:48 -0600

    create stream callback and queue in stream chain to make each chain has its own que and callback handler

--- app/chat/streaming/stream_handler.py
+++ app/chat/streaming/stream_handler.py
@@ -1,43 +1,23 @@
 from typing import Any
 from uuid import UUID
+from queue import Queue
 from langchain.callbacks.base import BaseCallbackHandler
-from langchain_core.outputs import ChatGenerationChunk, GenerationChunk, LLMResult
 
 
 class StreamCallbackHandler(BaseCallbackHandler):
-    def on_llm_new_token(
-        self,
-        token: str,
-        *,
-        chunk: GenerationChunk | ChatGenerationChunk | None = None,
-        run_id: UUID,
-        parent_run_id: UUID | None = None,
-        **kwargs: Any
-    ) -> Any:
-        return super().on_llm_new_token(
-            token, chunk=chunk, run_id=run_id, parent_run_id=parent_run_id, **kwargs
-        )
+    queue: Queue
 
-    def on_llm_end(
-        self,
-        response: LLMResult,
-        *,
-        run_id: UUID,
-        parent_run_id: UUID | None = None,
-        **kwargs: Any
-    ) -> Any:
-        return super().on_llm_end(
-            response, run_id=run_id, parent_run_id=parent_run_id, **kwargs
-        )
+    def __init__(self, queue: Queue) -> None:
+        self.queue = queue
+        super().__init__()
 
-    def on_llm_error(
-        self,
-        error: BaseException,
-        *,
-        run_id: UUID,
-        parent_run_id: UUID | None = None,
-        **kwargs: Any
-    ) -> Any:
-        return super().on_llm_error(
-            error, run_id=run_id, parent_run_id=parent_run_id, **kwargs
-        )
+    def on_llm_new_token(self, token: str, **kwargs: Any) -> Any:
+        self.queue.put(token)
+
+    def on_llm_end(self, **kwargs: Any) -> Any:
+        print("the LLM has finished current stream")
+        self.queue.put("--Done--")
+
+    def on_llm_error(self, error: BaseException, **kwargs: Any) -> Any:
+        print("the LLM met an error: %s", error)
+        self.queue.put("--Done--")
--- app/chat/streaming/streamable_chain.py
+++ app/chat/streaming/streamable_chain.py
@@ -1,14 +1,17 @@
 from typing import Any, Dict, Iterator
-from langchain_core.runnables.config import RunnableConfig
+from app.chat.streaming.stream_handler import StreamCallbackHandler
+from queue import Queue
 
 
 class StreamableChain:
 
     def stream(
-        self,
-        input: Dict[str, Any],
-        config: RunnableConfig | None = None,
-        **kwargs: Any | None
+        self, input: Dict[str, Any], **kwargs: Any | None
     ) -> Iterator[Dict[str, Any]]:
-        self(input)
-        return super().stream(input, config, **kwargs)
+        token_queue = Queue()
+        stream_callback = StreamCallbackHandler(queue=token_queue)
+
+        self(input, callback=[stream_callback])
+
+        while True:
+            yield token_queue.get()
