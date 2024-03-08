91b17451eff137c2ffb9f973279b45efac341d23
xianning liu <xianlinbox@gmail.com>
2024-03-07 19:06:44 -0600

    pass flask app context to task Thread

--- app/chat/streaming/streamable_chain.py
+++ app/chat/streaming/streamable_chain.py
@@ -1,6 +1,8 @@
 from typing import Any, Dict, Iterator
 from queue import Queue
 from threading import Thread
+from flask import current_app
+from flask.ctx import AppContext
 from app.chat.streaming.stream_handler import StreamCallbackHandler
 from app.chat.streaming.stream_handler import END_SIGNAL
 
@@ -13,10 +15,11 @@ class StreamableChain:
         token_queue = Queue()
         stream_callback = StreamCallbackHandler(queue=token_queue)
 
-        def task():
+        def task(app_context: AppContext):
+            app_context.push()
             self(input, callback=[stream_callback])
 
-        Thread(target=task).start()
+        Thread(target=task, args=[current_app.app_context()]).start()
 
         while True:
             token = token_queue.get()
