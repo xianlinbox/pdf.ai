178be1bbce8f6b3098b1e8576af1f8e113a996e2
xianning liu <xianlinbox@gmail.com>
2024-03-07 19:41:51 -0600

    separate LLM model for condense chain to make it not be cleaned by stream handler

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,3 +1,4 @@
+from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
 from app.chat.vector_store.pinecone import build_retriever
 from app.chat.memories.sql_message_memory import build_memory
@@ -9,6 +10,7 @@ def build_chat(chat_args: ChatArgs):
     retriever = build_retriever(chat_args=chat_args)
     memory = build_memory(chat_args=chat_args)
     llm = build_llms(chat_args=chat_args)
+    condense_llm = ChatOpenAI(streaming=False)
     return CoversationalStreamableChain.from_llm(
-        memory=memory, retriever=retriever, llm=llm
+        memory=memory, retriever=retriever, llm=llm, condense_question_llm=condense_llm
     )
