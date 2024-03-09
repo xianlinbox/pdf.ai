cbcb6a392f874e21560f13397fbfff3946004471
xianning liu <xianlinbox@gmail.com>
2024-03-08 20:14:27 -0600

    add llm_maps for multiple llm

--- app/chat/llms/open_ai.py
+++ app/chat/llms/open_ai.py
@@ -1,6 +1,13 @@
 from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
+from functools import partial
 
 
 def build_llms(chat_args: ChatArgs):
     return ChatOpenAI(streaming=chat_args.streaming)
+
+
+llm_maps = {
+    "gpt-4": partial(build_llms, model_name="gpt-4"),
+    "gpt-3.5-turbo": partial(build_llms, model_name="gpt-3.5-turbo"),
+}
