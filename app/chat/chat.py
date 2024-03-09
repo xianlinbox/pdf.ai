6f41e8cfcc8d84cdc0453a2cd7d2272ac7efe272
xianning liu <xianlinbox@gmail.com>
2024-03-08 20:41:29 -0600

    add pick llm, retriever, memory logic

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -1,15 +1,40 @@
+import random
 from langchain.chat_models import ChatOpenAI
 from app.chat.models import ChatArgs
-from app.chat.vector_store.pinecone import build_retriever
-from app.chat.memories.sql_message_memory import build_memory
-from app.chat.llms.open_ai import build_llms
+from app.web.api import get_conversation_components, set_conversation_components
+from app.chat.vector_store.pinecone import retrievers_map
+from app.chat.memories.sql_message_memory import memories_map
+from app.chat.llms.open_ai import llms_map
 from app.chat.streaming.conversational_stream_chain import CoversationalStreamableChain
 
 
+def select_component(type, component_map: map, chat_args: ChatArgs):
+    components_in_use = get_conversation_components(chat_args.conversation_id)
+    component_name = components_in_use[type]
+    if component_name is None:
+        component_picked_name = random.choice(list(component_map.keys()))
+        return component_picked_name, component_map[component_picked_name](chat_args)
+    return component_name, component_map[component_name](chat_args)
+
+
 def build_chat(chat_args: ChatArgs):
-    retriever = build_retriever(chat_args=chat_args)
-    memory = build_memory(chat_args=chat_args)
-    llm = build_llms(chat_args=chat_args)
+
+    retriever_name, retriever = select_component(
+        type="retriever", component_map=retrievers_map, chat_args=chat_args
+    )
+    memory_name, memory = select_component(
+        type="memory", component_map=memories_map, chat_args=chat_args
+    )
+    llm_name, llm = select_component(
+        type="llm", component_map=llms_map, chat_args=chat_args
+    )
+
+    set_conversation_components(
+        conversation_id=chat_args.conversation_id,
+        llm=llm_name,
+        retriever=retriever_name,
+        memory=memory_name,
+    )
     condense_llm = ChatOpenAI(streaming=False)
     return CoversationalStreamableChain.from_llm(
         memory=memory, retriever=retriever, llm=llm, condense_question_llm=condense_llm
--- app/chat/llms/open_ai.py
+++ app/chat/llms/open_ai.py
@@ -7,7 +7,7 @@ def build_llms(chat_args: ChatArgs):
     return ChatOpenAI(streaming=chat_args.streaming)
 
 
-llm_maps = {
+llms_map = {
     "gpt-4": partial(build_llms, model_name="gpt-4"),
     "gpt-3.5-turbo": partial(build_llms, model_name="gpt-3.5-turbo"),
 }
