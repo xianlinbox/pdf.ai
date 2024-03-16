039ae6c24d0f04ce1591f2c99cb84a1f14e32949
xianning liu <xianlinbox@gmail.com>
2024-03-15 19:17:52 -0500

    append tracing callback

--- app/chat/streaming/tracable_chain.py
+++ app/chat/streaming/tracable_chain.py
@@ -5,7 +5,10 @@ from app.chat.tracing.lang_fuse import fuse_client
 
 class TracableChain:
     def __call__(self, *args: Any, **kwds: Any) -> Any:
-        fuse_client.trace(
-            CreateTrace(id=self.metadata["conversation_id"], meta_data=self.metadata)
+        trace = fuse_client.trace(
+            CreateTrace(id=self.meta_data["conversation_id"], meta_data=self.meta_data)
         )
+        callbacks = kwds.get("callbacks", [])
+        callbacks.append(trace.getNewHandler())
+        kwds["callbacks"] = callbacks
         return super.__call__(*args, **kwds)
